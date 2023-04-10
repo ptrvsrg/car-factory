55cbdaefeae944659505558bd1062c2e39d76a88
ptrvsrg <s.petrov1@g.nsu.ru>
2023-04-10 18:46:09 +0700

    Add dealer

---
+++ src/main/java/ru/nsu/ccfit/petrov/task4/factory/dealer/Dealer.java
@@ -0,0 +1,48 @@
+package ru.nsu.ccfit.petrov.task4.factory.dealer;
+
+import java.util.UUID;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import ru.nsu.ccfit.petrov.task4.factory.product.Car;
+import ru.nsu.ccfit.petrov.task4.factory.storage.Storage;
+
+@Log4j2
+public class Dealer
+    extends Thread {
+
+    private final UUID id;
+    private final Storage<Car> carStorage;
+    @Setter
+    private int saleTime;
+
+    public Dealer(Storage<Car> carStorage, int saleTime) {
+        this.id = UUID.randomUUID();
+        this.carStorage = carStorage;
+        this.saleTime = saleTime;
+    }
+
+    /**
+     * If this thread was constructed using a separate {@code Runnable} run object, then that
+     * {@code Runnable} object's {@code run} method is called; otherwise, this method does nothing
+     * and returns.
+     * <p>
+     * Subclasses of {@code Thread} should override this method.
+     */
+    @Override
+    public void run() {
+        while (true) {
+            if (saleTime > 0) {
+                try {
+                    Thread.sleep(saleTime);
+
+                } catch (InterruptedException e) {
+                    log.warn(e);
+                    return;
+                }
+            }
+
+            Car car = carStorage.takeProduct();
+            log.info("Dealer " + id + " bought car" + car.getId());
+        }
+    }
+}
