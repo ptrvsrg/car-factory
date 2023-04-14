bb364e39eda05b08a8067cf61db7caa957babf0a
ptrvsrg <s.petrov1@g.nsu.ru>
2023-04-14 21:54:11 +0700

    Delete unused field

--- src/main/java/ru/nsu/ccfit/petrov/task4/factory/worker/Worker.java
+++ src/main/java/ru/nsu/ccfit/petrov/task4/factory/worker/Worker.java
@@ -1,10 +1,6 @@
 package ru.nsu.ccfit.petrov.task4.factory.worker;
 
-import java.util.UUID;
 import ru.nsu.ccfit.petrov.task4.threadpool.TaskThread;
 
 public class Worker
-    extends TaskThread {
-
-    private final UUID id = UUID.randomUUID();
-}
+    extends TaskThread {}
