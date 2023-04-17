2f1c642c5758adb4d6cde397dd3b71c9d7ccef73
ptrvsrg <s.petrov1@g.nsu.ru>
2023-04-17 18:52:17 +0700

    Print log about car assembling

--- src/main/java/ru/nsu/ccfit/petrov/task4/factory/worker/AssemblingTask.java
+++ src/main/java/ru/nsu/ccfit/petrov/task4/factory/worker/AssemblingTask.java
@@ -1,6 +1,7 @@
 package ru.nsu.ccfit.petrov.task4.factory.worker;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import ru.nsu.ccfit.petrov.task4.factory.product.Body;
 import ru.nsu.ccfit.petrov.task4.factory.product.Car;
 import ru.nsu.ccfit.petrov.task4.factory.product.Engine;
@@ -8,6 +9,7 @@ import ru.nsu.ccfit.petrov.task4.factory.product.SeatCover;
 import ru.nsu.ccfit.petrov.task4.factory.storage.Storage;
 import ru.nsu.ccfit.petrov.task4.threadpool.Task;
 
+@Log4j2
 @RequiredArgsConstructor
 public class AssemblingTask
     implements Task {
@@ -26,5 +28,6 @@ public class AssemblingTask
         Car car = new Car(engine, body, seatCover);
 
         carStorage.putProduct(car);
+        log.info(String.format("%s assembly %s", Thread.currentThread(), car));
     }
 }
