31ba3fbeb359b4f2cad08c293b68a5c1224c0398
ptrvsrg <s.petrov1@g.nsu.ru>
2023-04-22 16:29:55 +0700

    Add toString

--- src/main/java/ru/nsu/ccfit/petrov/task4/factory/dealer/Dealer.java
+++ src/main/java/ru/nsu/ccfit/petrov/task4/factory/dealer/Dealer.java
@@ -47,8 +47,8 @@ public class Dealer
             }
 
             Car car = carStorage.takeProduct();
-            if (car != null ) {
-                log.info(String.format("Dealer <%s> bought %s", id, car));
+            if (car != null) {
+                log.info(String.format("%s bought %s", this, car));
             }
         }
     }
